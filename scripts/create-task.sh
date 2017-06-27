11831e208217ad5212997339f8cdecb9713985e8
rowlandhill <hill.rowland@gmail.com>
2017-06-27 17:21:44 -0400

    Adds curl script for create-task
    Adds successful curl script to create task
    
    Next step on back end is to create script to delete projects

--- scripts/create-task.sh
+++ scripts/create-task.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+STRING="test" TEXT="this is the text" BOOLEAN="false" INTEGER=8 TOKEN="BAhJIiUxZmY5MTVjYzcyYjYwMzM0NDIxMDBkYmJlMzY4OWJhZgY6BkVG--3aa35a303cf8410db5221df87c9661e9ed0d2914"
+
 API="${API_ORIGIN:-http://localhost:4741}"
 URL_PATH="/tasks"
 curl "${API}${URL_PATH}" \
@@ -8,10 +10,11 @@ curl "${API}${URL_PATH}" \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=$TOKEN" \
   --data '{
-    "tasks": {
+    "task": {
       "name": "'"${STRING}"'",
       "description": "'"${TEXT}"'",
-      "iscomplete": "'"${BOOLEAN}"'"
+      "iscomplete": "'"${BOOLEAN}"'",
+      "project_id": "'"${INTEGER}"'"
     }
   }'
 
